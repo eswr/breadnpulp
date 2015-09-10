692161f12b8871aaced582dfe1aac3e979264172
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-09-11 02:02:12 +0530

    user validation errors sorted

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,13 +27,13 @@ class User < ActiveRecord::Base
 
 	VALID_PHONE_NUMBER_REGEX = /\A[1-9]{1}\d{9}\z/
 	validates :phone_number, 			presence: true,
-										length: { maximum: 255 },
+										length: { maximum: 10 },
 										format: { with: VALID_PHONE_NUMBER_REGEX },
 										uniqueness: true
 	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, 					presence: true,
-										length: { is: 10 },
+										length: { is: 255 },
 										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
 
