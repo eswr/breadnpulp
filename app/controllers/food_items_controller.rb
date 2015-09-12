a268cd5edbee9c77138f5c8aafe9ba6cfa967dc6
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-09-12 15:44:19 +0530

    checking logged in user

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -1,5 +1,6 @@
 class FoodItemsController < ApplicationController
 
+	before_action		:logged_in_user
 	before_action 		:admin_user
 	def new
 		@food_item = FoodItem.new
@@ -54,5 +55,14 @@ class FoodItemsController < ApplicationController
 		def admin_user
 			redirect_to root_path unless current_user.admin?
 		end
+
+		# Confirms a logged-in user.
+	    def logged_in_user
+	      unless logged_in?
+	        store_location
+	        flash[:danger] = "Please log in."
+	        redirect_to login_path
+	      end
+	    end
 
 end
