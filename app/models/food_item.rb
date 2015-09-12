7126f571f6c8d611b1cc9f6a94fb386835cb08ba
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-09-13 01:44:10 +0530

    useless commit

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -2,13 +2,18 @@
 #
 # Table name: food_items
 #
-#  id          :integer          not null, primary key
-#  name        :string
-#  course      :string
-#  veg_non_egg :string
-#  description :text
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string
+#  course             :string
+#  veg_non_egg        :string
+#  description        :text
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  type               :string
 #
 
 class FoodItem < ActiveRecord::Base
@@ -28,5 +33,5 @@ class FoodItem < ActiveRecord::Base
 
 	has_attached_file :image,	styles: { medium: "300x300" }
 	validates_attachment_content_type :image, 	content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-						
+	
 end
--- test/models/food_item_test.rb
+++ test/models/food_item_test.rb
@@ -2,13 +2,18 @@
 #
 # Table name: food_items
 #
-#  id          :integer          not null, primary key
-#  name        :string
-#  course      :string
-#  veg_non_egg :string
-#  description :text
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string
+#  course             :string
+#  veg_non_egg        :string
+#  description        :text
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  type               :string
 #
 
 require 'test_helper'
