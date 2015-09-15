002619f7c9ba66eb6570c499f0a382ad4f96a06d
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-09-15 17:47:10 +0530

    kickerr parital

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   before_action :logged_in_user,        only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user,          only: [:show, :edit, :update]
-  before_action :admin_user,            only: [:edit, :update, :index, :destroy]
+  before_action :admin_user,            only: [:update, :index, :destroy]
   
   def new
     @user = User.new
@@ -88,6 +88,5 @@ class UsersController < ApplicationController
       res = Net::HTTP.start(url.host, url.port) {|http|
         http.request(req)
       }
-      puts res.body
     end
 end
