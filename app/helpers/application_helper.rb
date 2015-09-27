7819c9d4124f83b67dac74eb23d8d8760c854582
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-09-27 17:04:57 +0530

    time.current not time.zone.current

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
 
 	def active_menu_date
 		Time.zone = 'Chennai'
-		Time.zone.current.hour < 12 ? Date.today : Date.tomorrow
+		Time.current.hour < 12 ? Date.today : Date.tomorrow
 	end
 
 	def active_day(date)
