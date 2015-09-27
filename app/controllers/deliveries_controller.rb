656e218e1c03331aa4c137b8cf3fbf80c889cb64
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-09-27 16:48:30 +0530

    menus helper in applicaiton controller

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -8,7 +8,7 @@ class DeliveriesController < ApplicationController
 	def new
 		logged_in? ? @user = current_user : @user = User.new
 		@delivery = @user.deliveries.new
-		@available_menus = Menu.where(available_on: active_menu_date)
+		@available_menus = menus_on(active_menu_date)
 		@delivery_statuses = DeliveryStatus.all
 		@available_menus.count.times { @delivery.packs.build }
 	end
