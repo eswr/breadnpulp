60fa444fe5ee29c5aea347cdc8123bd1240fa8f9
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-10-12 10:49:46 +0530

    msg for praveen

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -15,7 +15,7 @@ class DeliveriesController < ApplicationController
 		end
 		@menus = menus_on(active_menu_date)
 		@date = active_menu_date
-		send_sms_to_admin "User on new order, " + @delivery.user.name, "basheer@breadnpulp.com"
+		send_sms_to_admin "User on new order, " + @delivery.user.name, "praveen@breadnpulp.com"
 		send_sms_to_admin "User on new order, " + @delivery.user.name, "shubham@breadnpulp.com"
 	end
 
@@ -29,12 +29,12 @@ class DeliveriesController < ApplicationController
 		if @delivery.save
 			flash[:success] = "Order successfully placed"
 			redirect_to @user
-			send_sms_to_admin flash[:success] + ", " + @delivery.user.name + ", " + @delivery.user.phone_number + ", " + @delivery.at.to_s, "basheer@breadnpulp.com"
+			send_sms_to_admin flash[:success] + ", " + @delivery.user.name + ", " + @delivery.user.phone_number + ", " + @delivery.at.to_s, "praveen@breadnpulp.com"
 			send_sms_to_admin flash[:success] + ", " + @delivery.user.name + ", " + @delivery.user.phone_number + ", " + @delivery.at.to_s, "shubham@breadnpulp.com"
 		else
 			flash.now[:danger] = "Order not placed. Please enter time and select an address"
 			render 'new'
-			send_sms_to_admin flash[:danger] + ", " + @delivery.user.name + ", " + @delivery.user.phone_number + ", " + @delivery.at.to_s, "basheer@breadnpulp.com"
+			send_sms_to_admin flash[:danger] + ", " + @delivery.user.name + ", " + @delivery.user.phone_number + ", " + @delivery.at.to_s, "praveen@breadnpulp.com"
 			send_sms_to_admin flash[:danger] + ", " + @delivery.user.name + ", " + @delivery.user.phone_number + ", " + @delivery.at.to_s, "shubham@breadnpulp.com"
 		end
 	end
