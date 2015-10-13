40e93a72b5e9a43252e01b28ee5d92f1daa8e2b5
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-10-13 14:47:35 +0530

    user can update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   before_action :logged_in_user,        only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user,          only: [:show, :edit, :update]
-  before_action :admin_user,            only: [:update, :index, :destroy]
+  before_action :admin_user,            only: [:index, :destroy]
   
   def new
     @user = User.new
