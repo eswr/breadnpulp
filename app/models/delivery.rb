bd41eec0d34d814e9e83dd3a3afd536573d17fcd
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-10-14 00:10:00 +0530

    useless

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -24,7 +24,7 @@ class Delivery < ActiveRecord::Base
   belongs_to :delivery_status
   belongs_to :payment_status
 
-  has_many :packs
+  has_many :packs, dependent: :destroy
   has_many :menus, through: :packs
 
   validates :delivery_date,		   presence: true
