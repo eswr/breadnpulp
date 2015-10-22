67a3577e43bae57083dc5448ac33f3d4b5328078
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-10-22 22:00:34 +0530

    subscription id bug 2

--- db/migrate/20151022153913_add_subscription_id_to_deliveries.rb
+++ db/migrate/20151022153913_add_subscription_id_to_deliveries.rb
@@ -1,6 +1,5 @@
 class AddSubscriptionIdToDeliveries < ActiveRecord::Migration
   def change
-  	remove_column :deliveries, :subscription_id, :integer
   	add_column :deliveries, :subscription_id, :integer
   end
 end
