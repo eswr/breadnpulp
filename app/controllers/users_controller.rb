14b2933f602f0a08fa70c602de985f92d3f83b91
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-11-02 22:43:31 +0530

    correct user for edit, update, show, show_orders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :logged_in_user,        only: [:index, :show, :edit, :update, :destroy]
-  before_action :correct_user
+  before_action :correct_user,          only: [:show, :edit, :update, :show_orders]
   before_action :admin_user,            only: [:index, :destroy]
   
   def new
