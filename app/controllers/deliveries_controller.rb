9ef70f0793f95f6259e1710d9c36b0a27ee81164
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-11-04 04:18:22 +0530

    missing end bug fix

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -99,6 +99,7 @@ class DeliveriesController < ApplicationController
 
 	def delivery_params
 		params.require(:delivery).permit(:user_id, :delivery_date, :at, :collect, :address_id, :delivery_status_id, :payment_status_id, :payment_date, :payment_mode, packs_attributes: [:id, :quantity, :menu_id, :unit_price, :payment_date, :payment_mode, :_destroy])
+	end
 
 	# def editable_delivery
 	# 	return true if current_user.admin?
