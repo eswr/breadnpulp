66965d22030262928f420b830eec9e6882778daf
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-11-13 13:10:56 +0530

    Ingredients2.csv

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -28,8 +28,7 @@ class FoodItem < ActiveRecord::Base
 	validates	:veg_non_egg,	presence: true,
 								length: { maximum: 20 }
 
-	validates	:description,	presence: true,
-								length: { maximum: 255 }
+	validates	:description,	length: { maximum: 255 }
 
 	has_attached_file :image,	styles: { medium: "300x300", thumb: "100x100" }
 	validates_attachment_content_type :image, 	content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
