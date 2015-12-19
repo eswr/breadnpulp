5cb43a2b14eca0f89e9654e69820c86e0ba13ba6
Syed Fazil Basheer <basheer@breadnpulp.com>
2015-12-19 18:46:16 +0530

    meta tag descroption

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,7 @@ class StaticPagesController < ApplicationController
 			menu.kickerr_name = menu.get_kickerr_name
 		end
 		@date = active_menu_date
+		@meta_description = "Healthy and delicious breakfast meals delivered to your doorstep everyday. Currently operating in Powai, Hiranandani, Chandivali, Andheri East, Sakinaka and Marol"
 	end
 
 	def about
