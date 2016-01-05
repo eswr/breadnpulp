bb32ffab549df9dfcd1bb668be605a1869bc2fe9
Syed Fazil Basheer <basheer@breadnpulp.com>
2016-01-06 03:23:06 +0530

    param bug

--- app/controllers/otps_controller.rb
+++ app/controllers/otps_controller.rb
@@ -1,32 +1,40 @@
 class OtpsController < ApplicationController
 
-  def new
-  	@phone_number = params[:otp][:phone_number]
-  	user = User.find_by(phone_number: @phone_number)
-  	user.send_otp
-  	if !@phone_number.match(/\A[1-9]{1}\d{9}\z/)
-  		flash[:warning] = "Not a valid number"
-  		redirect_to :back
-  	elsif !user
-  		flash[:warning] = "Number not found!"
-  		redirect_to :back
-  	end
-  end
+	before_action :check_if_number
 
-  def create
-  	user = User.find_by(phone_number: params[:otp][:phone_number])
-	if !user
-		flash[:danger] = "Phone number not found! Please try again."
-		redirect_to login_path
-	else
-		if user.authenticate_otp(params[:otp][:otp], drift: 60)
-			user.activate if !user.activated?
-			log_in user
-			redirect_to user
-		else
-			flash[:danger] = "OTP incorrect. Please try again."
-	      	redirect_to get_otp_path otp: { phone_number: params[:otp][:phone_number] }
-	    end
+	def new
+		@phone_number = params[:otp][:phone_number]
+		user = User.find_by(phone_number: @phone_number)
+		user.send_otp
+		if !@phone_number.match(/\A[1-9]{1}\d{9}\z/)
+			flash[:warning] = "Not a valid number"
+			redirect_to :back
+		elsif !user
+			flash[:warning] = "Number not found!"
+			redirect_to :back
+		end
 	end
-  end
+
+	def create
+		user = User.find_by(phone_number: params[:otp][:phone_number])
+		if !user
+			flash[:danger] = "Phone number not found! Please try again."
+			redirect_to login_path
+		else
+			if user.authenticate_otp(params[:otp][:otp], drift: 60)
+				user.activate if !user.activated?
+				log_in user
+				redirect_to user
+			else
+				flash[:danger] = "OTP incorrect. Please try again."
+		      	redirect_to get_otp_path otp: { phone_number: params[:otp][:phone_number] }
+		    end
+		end
+	end
+
+	private
+
+		def check_if_number
+			redirect_to login_path if params[:otp][:phone_number].nil?
+		end
 end
